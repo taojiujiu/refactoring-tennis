049713e4aa8e8862d72e7e495ec0009836d73268
YuLing Liu <yuling.liu@thoughtworks.com>
2020-03-02 23:37:21 +0800

    Simple refactoring

--- src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
@@ -4,39 +4,32 @@ public class TennisGame1 implements TennisGame {
 
     private int playScore1 = 0;
     private int playScore2 = 0;
-    private String player1Name;
-    private String player2Name;
-
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals("player1"))
             playScore1 += 1;
         else
             playScore2 += 1;
     }
 
     public String getScore() {
-        String score = "";
+        StringBuilder score = new StringBuilder();
         int tempScore=0;
         if (playScore1 == playScore2)
         {
             switch (playScore1)
             {
                 case 0:
-                        score = "Love-All";
+                        score = new StringBuilder("Love-All");
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                        score = new StringBuilder("Fifteen-All");
                     break;
                 case 2:
-                        score = "Thirty-All";
+                        score = new StringBuilder("Thirty-All");
                     break;
                 default:
-                        score = "Deuce";
+                        score = new StringBuilder("Deuce");
                     break;
 
             }
@@ -44,34 +37,34 @@ public class TennisGame1 implements TennisGame {
         else if (playScore1 >=4 || playScore2 >=4)
         {
             int minusResult = playScore1 - playScore2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            if (minusResult==1) score = new StringBuilder("Advantage player1");
+            else if (minusResult ==-1) score = new StringBuilder("Advantage player2");
+            else if (minusResult>=2) score = new StringBuilder("Win for player1");
+            else score = new StringBuilder("Win for player2");
         }
         else
         {
             for (int i=1; i<3; i++)
             {
                 if (i==1) tempScore = playScore1;
-                else { score+="-"; tempScore = playScore2;}
+                else { score.append("-"); tempScore = playScore2;}
                 switch(tempScore)
                 {
                     case 0:
-                        score+="Love";
+                        score.append("Love");
                         break;
                     case 1:
-                        score+="Fifteen";
+                        score.append("Fifteen");
                         break;
                     case 2:
-                        score+="Thirty";
+                        score.append("Thirty");
                         break;
                     case 3:
-                        score+="Forty";
+                        score.append("Forty");
                         break;
                 }
             }
         }
-        return score;
+        return score.toString();
     }
 }
--- src/main/java/cn/xpbootcamp/tennis/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame2.java
@@ -7,13 +7,6 @@ public class TennisGame2 implements TennisGame
 
     private String P1res = "";
     private String P2res = "";
-    private String player1Name;
-    private String player2Name;
-
-    public TennisGame2(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
 
     public String getScore(){
         String score = "";
