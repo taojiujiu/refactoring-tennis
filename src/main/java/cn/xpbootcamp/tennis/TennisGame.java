fc1f62bd38a587382f331b6e83831a649852f290
YuLing Liu <yuling.liu@thoughtworks.com>
2020-03-03 21:15:21 +0800

    Refactor lazy element

--- src/main/java/cn/xpbootcamp/tennis/TennisGame.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame.java
@@ -13,47 +13,38 @@ public class TennisGame {
     }
 
     public String getScore() {
-        StringBuilder score = new StringBuilder();
-        int tempScore = 0;
         if (player1Score == player2Score) {
             switch (player1Score) {
                 case 0:
-                    score = new StringBuilder("Love-All");
-                    break;
+                    return "Love-All";
                 case 1:
-                    score = new StringBuilder("Fifteen-All");
-                    break;
+                    return "Fifteen-All";
                 case 2:
-                    score = new StringBuilder("Thirty-All");
-                    break;
+                    return "Thirty-All";
                 default:
-                    score = new StringBuilder("Deuce");
-                    break;
-
+                    return "Deuce";
             }
         } else if (player1Score >= 4 || player2Score >= 4) {
             int minusResult = player1Score - player2Score;
-            if (minusResult == 1) score = new StringBuilder("Advantage player1");
-            else if (minusResult == -1) score = new StringBuilder("Advantage player2");
-            else if (minusResult >= 2) score = new StringBuilder("Win for player1");
-            else score = new StringBuilder("Win for player2");
+            if (minusResult == 1) return "Advantage player1";
+            else if (minusResult == -1) return "Advantage player2";
+            else if (minusResult >= 2) return "Win for player1";
+            return "Win for player2";
         } else {
-
-            score = transformScore(player1Score).append("-").append(transformScore(player2Score));
+            return transformScore(player1Score) + "-" + transformScore(player2Score);
         }
-        return score.toString();
     }
 
-    private StringBuilder transformScore(int tempScore) {
+    private String transformScore(int tempScore) {
         switch (tempScore) {
             case 0:
-                return new StringBuilder("Love");
+                return "Love";
             case 1:
-                return new StringBuilder("Fifteen");
+                return "Fifteen";
             case 2:
-                return new StringBuilder("Thirty");
+                return "Thirty";
             case 3:
-                return new StringBuilder("Forty");
+                return "Forty";
             default:
                 throw new RuntimeException("");
         }
