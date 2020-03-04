5f9d6c0cc2c818da3c3647ad9c9ec139f9300c71
YuLing Liu <yuling.liu@thoughtworks.com>
2020-03-04 12:58:15 +0800

    Refactoring long function

--- src/main/java/cn/xpbootcamp/tennis/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame2.java
@@ -7,44 +7,44 @@ public class TennisGame2 implements TennisGame
 
     public String getScore(int P1point,int P2point){
         String score = "";
-        if (P1point == P2point && P1point < 4)
+        score = equalCase(P1point, P2point, score);
+
+        score = hasZeroCase(P1point, P2point, score);
+
+        score = littleScoreCase(P1point, P2point, score);
+
+        score = advantageCase(P1point, P2point, score);
+
+        score = winCase(P1point, P2point, score);
+        return score;
+    }
+
+    private String winCase(int P1point, int P2point, String score) {
+        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
         {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
-            score += "-All";
+            score = "Win for player1";
         }
-        if (P1point==P2point && P1point>=3)
-            score = "Deuce";
-
-        if (P1point > 0 && P2point==0)
+        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
         {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-
-            P2res = "Love";
-            score = P1res + "-" + P2res;
+            score = "Win for player2";
         }
-        if (P2point > 0 && P1point==0)
-        {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
+        return score;
+    }
 
-            P1res = "Love";
-            score = P1res + "-" + P2res;
+    private String advantageCase(int P1point, int P2point, String score) {
+        if (P1point > P2point && P2point >= 3)
+        {
+            score = "Advantage player1";
         }
 
+        if (P2point > P1point && P1point >= 3)
+        {
+            score = "Advantage player2";
+        }
+        return score;
+    }
+
+    private String littleScoreCase(int P1point, int P2point, String score) {
         if (P1point>P2point && P1point < 4)
         {
             if (P1point==2)
@@ -69,25 +69,50 @@ public class TennisGame2 implements TennisGame
                 P1res="Thirty";
             score = P1res + "-" + P2res;
         }
+        return score;
+    }
 
-        if (P1point > P2point && P2point >= 3)
+    private String hasZeroCase(int P1point, int P2point, String score) {
+        if (P1point > 0 && P2point==0)
         {
-            score = "Advantage player1";
-        }
+            if (P1point==1)
+                P1res = "Fifteen";
+            if (P1point==2)
+                P1res = "Thirty";
+            if (P1point==3)
+                P1res = "Forty";
 
-        if (P2point > P1point && P1point >= 3)
-        {
-            score = "Advantage player2";
+            P2res = "Love";
+            score = P1res + "-" + P2res;
         }
+        if (P2point > 0 && P1point==0)
+        {
+            if (P2point==1)
+                P2res = "Fifteen";
+            if (P2point==2)
+                P2res = "Thirty";
+            if (P2point==3)
+                P2res = "Forty";
 
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
-        {
-            score = "Win for player1";
+            P1res = "Love";
+            score = P1res + "-" + P2res;
         }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
+        return score;
+    }
+
+    private String equalCase(int P1point, int P2point, String score) {
+        if (P1point == P2point && P1point < 4)
         {
-            score = "Win for player2";
+            if (P1point==0)
+                score = "Love";
+            if (P1point==1)
+                score = "Fifteen";
+            if (P1point==2)
+                score = "Thirty";
+            score += "-All";
         }
+        if (P1point==P2point && P1point>=3)
+            score = "Deuce";
         return score;
     }
 }
